4c5323ce7417beee54f6d6a2f07ae60ff2da4cda
VAISHALI PISAL <bhushanpisal620@gmail.com>
2024-03-30 19:53:01 +0530

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,33 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import authRoutes from './routes/authRoutes.js'
+import eventRoutes from './routes/eventRoutes.js'
+import eventModel from "./models/eventModel.js";
+import userModel from "./models/userModel.js";
+const app = express();
+
+const corsOptions = {
+  origin: 'http://localhost:5173',
+};
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/auth',authRoutes)
+app.use('/api/event',eventRoutes)
+
+
+mongoose
+  .connect("mongodb://localhost:27017/eventDB")
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err.message);
+  });
+
+const PORT = 8000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
